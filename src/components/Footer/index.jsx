0073410b52efdaa5af8c2b80e00e3e237a8df9f1
Andy <84168944+AZurek17@users.noreply.github.com>
2023-11-30 20:19:14 -0700

    added files to start with

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,11 @@
+import "./style.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <span>Zurek Portfolio 2023</span>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/main.jsx
@@ -0,0 +1,38 @@
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import "./index.css";
+
+import App from './App';
+import About from "./pages/About";
+import Portfolio from "./pages/Portfolio";
+import Contact from "./pages/Contact";
+import Resume from "./pages/Resume";
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <About />,
+      },
+      {
+        path: '/Portfolio',
+        element: <Portfolio />,
+      },
+      {
+        path: '/Contact',
+        element: <Contact />,
+      },
+      {
+        path: '/Resume',
+        element: <Resume />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+);
