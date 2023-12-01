214387797b6a983812e8614bddc41b12158a29dc
Andy <84168944+AZurek17@users.noreply.github.com>
2023-11-30 22:50:13 -0700

    updated Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,84 +1,101 @@
-import React, { Component } from "react";
 import { useState } from 'react';
 import "../styles/SearchForm/style.css";
-import "../styles/SearchResults/style.css";
-import { checkPassword, validateEmail } from '../../utils/helpers';
+import { validateEmail } from '../utils/helpers';
 
-class Search extends Component {
-  state = {
-    search: "",
-    breeds: [],
-    results: [],
-    error: "",
+function Contact() {
+  // Create state variables for the fields in the form
+  // We are also setting their initial values to an empty string
+  const [email, setEmail] = useState('');
+  const [userName, setUserName] = useState('');
+  const [message, setMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  
+  const handleInputChange = (e) => {
+    // Getting the value and name of the input which triggered the change
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
+
+    // Based on the input type, we set the state of either email, username, and password
+    if (inputType === 'email') {
+      setEmail(inputValue);
+    } else if (inputType === 'userName') {
+      setUserName(inputValue);
+    } else {
+      setMessage(inputValue);
+    }
   };
 
-  // When the component mounts, get a list of all available base breeds and update this.state.breeds
-  componentDidMount() {
-    API.getBaseBreedsList()
-      .then((res) => this.setState({ breeds: res.data.message }))
-      .catch((err) => console.log(err));
+  const handleFormSubmit = (e) => {
+    // Preventing the default behavior of the form submit (which is to refresh the page)
+    e.preventDefault();
+
+    // First we check to see if the email is not valid or if the userName is empty. If so we set an error message to be displayed on the page.
+    if (!validateEmail(email) || !userName) {
+      setErrorMessage('🤔 Email is invalid 🤔');
+    if (!userName) {
+      setErrorMessage('🤔 No username entered 🤔');
+    if (!message) {
+      setErrorMessage('🤔 No Message entered 🤔');
+      // We want to exit out of this code block if something is wrong so that the user can correct it
+      return;
+      
+    }
+  }
+}
+    
+    // If everything goes according to plan, we want to clear out the input after a successful registration.
+    setUserName('');
+    setMessage('');
+    setEmail('');
+    // setformInfo({email: "",userName: "",password: "",errorMessage: "" })
   }
 
-  handleInputChange = (event) => {
-    this.setState({ search: event.target.value });
-  };
-
-  handleFormSubmit = (event) => {
-    event.preventDefault();
-    API.getDogsOfBreed(this.state.search)
-      .then((res) => {
-        if (res.data.status === "error") {
-          throw new Error(res.data.message);
-        }
-        this.setState({ results: res.data.message, error: "" });
-      })
-      .catch((err) => this.setState({ error: err.message }));
-  };
-  render() {
     return (
-      <div>
-        <div className={`container`} style={{ minHeight: "80%" }}>
-          <h1 className="text-center">Search By Breed!</h1>
-          <div
-            role="alert"
-            className={`alert alert-danger fade in`}
-            style={{
-              width: "80%",
-              margin: "0 auto",
-              marginTop: 18,
-              opacity: this.state.error ? 1 : 0,
-              marginBottom: 10,
-            }}
-          >
-            {this.state.error}
+      <div className="container text-center">
+        <h1>😁 Send me a Message 😁 {userName}</h1>
+        <form className="form" onSubmit={handleFormSubmit}>
+        <div className="container text-left">
+        Name:
+        <input
+            value={userName}
+            name="userName"
+            onChange={handleInputChange}
+            // onChange={orThisWayForAll}
+            type="text"
+            placeholder="Enter your name here"
+          />
+          Email:
+          <input
+            value={email}
+            name="email"
+            onChange={handleInputChange}
+            // onChange={orThisWayForAll}
+            type="email"
+            placeholder="Enter your email here"
+          />
+          Message:
+          <input
+            value={message}
+            name="message"
+            onChange={handleInputChange}
+            // onChange={orThisWayForAll}
+            type="message"
+            placeholder="Enter a short message here"
+          />
           </div>
-          <form className="search">
-            <div className="form-group">
-              <label htmlFor="name">first Name:</label>
-              <input
-                value={this.state.search}
-                onChange={this.handleInputChange}
-                name="name"
-                list="names"
-                type="text"
-                className="form-control"
-                placeholder="name"
-                id="name"
-              />
-              <button
-                type="submit"
-                onClick={this.handleFormSubmit}
-                className="btn btn-success"
-              >
-                Send Message
-              </button>
-            </div>
-          </form>
+          <button type="submit">Send Message</button>
           
-        </div>
+        </form>
+        {errorMessage && (
+          <div>
+            <p className="error-text">{errorMessage}</p>
+          </div>
+        )}
       </div>
     );
   }
-}
 
-export default Search;
+
+
+export default Contact;
